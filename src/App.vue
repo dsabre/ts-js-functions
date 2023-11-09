cef082d623e31e77077c3c6615b88e7584ce1ba1
Daniele Sabre <daniele.sabre@gmail.com>
2023-11-09 11:27:11 +0100

    added flowbite

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+import Sidebar from '@/components/Sidebar.vue';
 </script>
 
 <template>
+
+
   <header>
+    <Sidebar/>
+
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,6 @@
 
 <template>
   <main>
-    welcome
+    TheWelcome
   </main>
 </template>
